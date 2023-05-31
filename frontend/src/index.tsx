c4d0511aefb09e3f47e9d9f228a8aa3ab5e0be85
Elena Kroupkin <pocoat@gmail.com>
2023-05-31 13:29:07 +1200

    ESLint + Prettier config

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,4 +6,4 @@ const container = document.getElementById('app-root')!
 const root = createRoot(container!)
 // const root = createRoot(container)
 
-root.render(<h1>Hello from index.ts!</h1>)
+root.render(<h1>Hello from index. ts!</h1>)
