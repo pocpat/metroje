a059f891b33d480a974c339e479c689fc28666f5
JojorioCh <josephch@missionreadyhq.com>
2023-06-01 23:10:00 +1200

    Dockerfile and .dockerignore added

--- backend/src/router/users.ts
+++ backend/src/router/users.ts
@@ -4,10 +4,10 @@ import {
   getAllUsers,
   updateUser,
 } from "../controllers/userController";
-import { isAuthenticated, isOwner } from "../middlewares";
+// import { isAuthenticated, isOwner } from "../middlewares";
 
 export default (router: express.Router) => {
-  router.get("/users", isAuthenticated, getAllUsers);
-  router.delete("/users/:id", isAuthenticated, isOwner, deleteUser);
-  router.patch("/users/:id", isAuthenticated, isOwner, updateUser);
+  router.get("/users", /*isAuthenticated,*/ getAllUsers);
+  router.delete("/users/:id", /*isAuthenticated, isOwner,*/ deleteUser);
+  router.patch("/users/:id", /*isAuthenticated, isOwner,*/ updateUser);
 };
