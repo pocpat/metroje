c1b6d962a1f11874e3d8687f53127a5778dccb07
Elena Kroupkin <pocoat@gmail.com>
2023-06-06 01:14:29 +1200

    API POST preparation

--- frontend/src/common/searchpanel/SearchPanel.tsx
+++ frontend/src/common/searchpanel/SearchPanel.tsx
@@ -1,15 +1,60 @@
 import React from 'react';
 import '../../styles/SearchPanel.css';
 import BedroomsSelectLabels from './BedroomsFadeMenu';
-import LocationFadeMenu from './LocationFadeMenu';
 import PropertytypeSelectLabels from './PropertytypeFadeMenu';
 import RentmaxSelectLabels from './RentmaxFadeMenu';
 import RentminSelectLabels from './RentminFadeMenu';
 
 
-
-
+interface FormData {
+  location: string;
+  suburb: string;
+  rentmin: number;
+  rentmax: number;
+  bedrooms: number;
+  propertytype: string;
+}
 const SearchPanel = () => {
+  // // const handleSubmit = () => {
+  //   // Get the values of the inputs and dropdown menus
+  //   const locationValue = document.querySelector('.spLocation input').value;
+  //   const suburbValue = document.querySelector('.spSuburb input').value;
+  //   const rentminValue = document.querySelector('.spRentMin input').value;
+  //   const rentmaxValue = document.querySelector('.spRentMax input').value;
+  //   const bedroomsValue = document.querySelector('.spBedrooms input').value;
+  //   const propertytypeValue = document.querySelector('.spPropertyType input').value;
+
+
+    // // Create an object with the form data
+    // const formData: FormData = {
+    //   location: locationValue,
+    //   suburb: suburbValue,
+    //   rentmin: rentminValue,
+    //   rentmax: rentmaxValue,
+    //   bedrooms: bedroomsValue,
+    //   propertytype: propertytypeValue
+    // };
+
+
+//  // Send the form data to the backend server
+//  fetch('http://localhost:8080/rents/filter', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify(formData)
+// })
+// .then(response => response.json())
+// .then(data => {
+//   // Handle the response from the backend server
+//   console.log(data);
+// })
+// .catch(error => {
+//   // Handle any errors
+//   console.error(error);
+// });
+// };
+
   return (
     <div className="spContainer">
       <p>Search our available listings</p>
@@ -38,10 +83,14 @@ const SearchPanel = () => {
         </div>
       </section>
       <section className="spSearchButton">
-        <button>Search</button>
+        <button type='submit' 
+        // onClick={handleSubmit}
+        >Search</button>
       </section>
     </div>
   );
 };
+
+
 
 export default SearchPanel;
