d099e7b20bb64d8418646a129b8b70a44ffc5e22
Elena Kroupkin <pocoat@gmail.com>
2023-06-09 11:31:18 +1200

    fix layeout section2 and 3 after merge

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -3,7 +3,7 @@ import Header from '../../common/header/Header';
 import HomeSection1 from './HomeSection1';
 import HomeSection2 from './HomeSection2';
 import HomeSection3 from './HomeSection3';
-// import HomeSection3Carousel from '../../common/Carousel/HomeSection3Carousel';
+import HomeSection3Carousel from '../../common/Carousel/HomeSection3Carousel';
 import HomeSection4 from './HomeSection4';
 import Footer from '../../common/footer/Footer';
 
