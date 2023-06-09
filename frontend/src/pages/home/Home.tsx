3fd09205901d3e4fb0c5b82de45d5920b0a6af84
JojorioCh <josephch@missionreadyhq.com>
2023-06-09 17:29:37 +1200

    what have I done here?

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Header from '../../common/header/Header';
 import HomeSection1 from './HomeSection1';
 import HomeSection2 from './HomeSection2';
-// import HomeSection3 from './HomeSection3';
-import HomeSection3Carousel from '../../common/Carousel/HomeSection3Carousel';
+import HomeSection3 from './HomeSection3';
+// import HomeSection3Carousel from '../../common/Carousel/HomeSection3Carousel';
 import HomeSection4 from './HomeSection4';
 import Footer from '../../common/footer/Footer';
 
@@ -14,11 +14,8 @@ const Home = () => {
       <Header />
       <HomeSection1 />
       <HomeSection2 />
-      <br />
-      <br />
-      <br />
-      {/* <HomeSection3 /> */}
-      <HomeSection3Carousel/>
+      <HomeSection3 />
+      {/* <HomeSection3Carousel/> */}
       <HomeSection4 />
       <Footer />
     </div>
