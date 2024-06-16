0a1b08f03ab845a686f04680eedbdc965f2b96ca
Elena Kroupkin <pocoat@gmail.com>
2024-06-17 11:57:50 +1200

    get data ( find({}) ) from MongoDB Atlas , no mongoose

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@
 // await mongoose.connect(uri);
 // console.log ("Connected to MongoDB Atlas from the index.js !");
 
-// ============================ V2 =========================
+// ============================ mongoose  V2 =========================
 // const mongoose = require('mongoose');
 // const env = require('dotenv');
 
@@ -25,7 +25,7 @@
 //     console.log('Error connecting to MongoDB Atlas:', err);
 //   });
 
-  // ============================ V3 =========================
+  // ============================ mongoose V3 =========================
 
 //  // import { connect, connection } from 'mongoose';
 //  import pkg from 'mongoose';
@@ -46,7 +46,7 @@
 //   console.log('Error connecting to MongoDB Atlas:', err);
 // });
 
-// ============================ V4 =========================
+// ============================ mongoose V4 =========================
 import mongoose from 'mongoose';
 import { config } from 'dotenv';
 //import { property } from 'lodash';
@@ -87,3 +87,46 @@ propertytype: String,
     console.error('Error fetching data from MongoDB:', err);
   }
 });
+
+// ============================ mongoDB Atlas V5 =========================
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import env from 'dotenv';
+
+// const uri = env.config().parsed?.MONGODB_URI ;
+console.log("uri: ", uri);
+  //'mongodb+srv://elenamongodb:<password>@cluster0.og4lx1s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    console.log('Connected to the server');
+  
+    // Send a ping to confirm a successful connection
+    await client.db('metroje').command({ ping: 1 });
+    console.log('Pinged the deployment');
+  
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+  
+    // Fetch all documents from a specific collection
+    const collection = client.db("metroje").collection("rents");
+    const docs = await collection.find({}).toArray();
+    console.log(docs);
+  } catch (err) {
+    console.error('Error:', err);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
